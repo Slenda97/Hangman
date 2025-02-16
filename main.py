42bb1d027d26805f1de84c99567b206329036a08
Slenda97 <donovanlomax97@gmail.com>
2025-02-16 12:35:12 +0000

    3) Initial word bank added, added if statements for multiple letter inputs

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
+from os import read
+import csv, random
+
 def main():
     word = get_word()
     num_of_guesses = 8
+    max_guesses = 20
     list_word = split_word(word)
     word_length = len(list_word)
     guess_word = []
     for i in range(word_length):
         guess_word.append("_")
-    for i in range(num_of_guesses):
+    for i in range(max_guesses):
         print(" ".join(guess_word))
-        guess = input("Enter a letter: ")
-        if guess in list_word:
-            for i in range(word_length):
-                if list_word[i] == guess:
-                    guess_word[i] = guess
+        guess = guess_letter()
+        if len(guess) > 1:
+            print("You must enter a single letter.")
+            break
         else:
-            num_of_guesses -= 1
-            print(f"Incorrect! You have {num_of_guesses} guesses left.")
+            if guess in list_word:
+                for i in range(word_length):
+                    if list_word[i] == guess:
+                        guess_word[i] = guess
+            else:
+                num_of_guesses -= 1
+                if num_of_guesses > 0:
+                    print(f"Incorrect! You have {num_of_guesses} guesses left.")
+                else:
+                    print(f"You lose! The correct word was {word}.")
+                    break
         if guess_word == list_word:
             print("You win!")
             break
     
-        
+def guess_letter():
+    guess = input("Enter a letter: ")
+    return guess    
     
 def get_word():
-    word = input("Enter a word: ")
+    word = read_file()
     if word == "":
         print("You must enter a word.")
         word = get_word()
@@ -32,6 +46,13 @@ def get_word():
 
 def split_word(word):
     return list(word)
-    
+
+def read_file():
+    with open("words.csv", "r") as file:
+        reader = csv.reader(file)
+        words = list(reader)
+        choice = random.choice(words)
+    return choice[0]
+        
 
 main()
